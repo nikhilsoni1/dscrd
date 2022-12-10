fc84322c4ee4d08ac6fada434be71de0217ad0be
nikhilsonilctwn <nikhilkamlesh.soni@gmail.com>
2022-12-10 15:02:40 -0800

    [FEATURE] DB storage for YT results

---
+++ yt_db.py
@@ -0,0 +1,72 @@
+import sqlalchemy as db
+import re
+import os
+import json
+import pandas as pd
+import datetime
+import shutil
+
+# Defining the Engine
+engine = db.create_engine('sqlite:///all_data.db', echo=True)
+  
+# Create the Metadata Object
+metadata_obj = db.MetaData()
+  
+# Define the profile table
+  
+# database name
+youtube = db.Table(
+    'youtube',                                        
+    metadata_obj,                                    
+    db.Column('created_at', db.TIMESTAMP, primary_key=True),  
+    db.Column('in_channel', db.String, primary_key=True),                    
+    db.Column('sent_by', db.String, primary_key=True),
+    db.Column('title', db.String, primary_key=True),
+    db.Column('url', db.String, primary_key=True)                
+)
+  
+ 
+
+# Create the profile table
+metadata_obj.create_all(engine)
+source = "yt_info"
+backup = os.path.join(source, "backup")
+source_exists = os.path.exists(source)
+os.makedirs(backup, exist_ok=True)
+
+if source_exists:
+    pass
+else:
+    print(f"source {source} doesn't exist!")
+    exit()
+
+pattern = re.compile(r"yt-\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}-\d{6}\.json")
+file_names = os.listdir(source)
+file_names1 = list(filter(lambda x: re.match(pattern, x), file_names))
+
+if len(file_names1) > 0:
+    pass
+else:
+    print("Nothing to process!")
+    exit()
+
+file_paths = list(map(lambda x: os.path.join(source, x), file_names1))
+json_store = list()
+for file in file_paths:
+    with open(file, "r") as fp:
+        j = json.load(fp)
+        json_store += j.copy()
+
+insert = db.insert(youtube).prefix_with("OR IGNORE")
+for j in json_store:
+    created_at = j["created_at"]
+    j["created_at"] = datetime.datetime.fromisoformat(created_at)
+    with engine.connect() as conn:
+        result = conn.execute(insert, j)
+        debug = True
+
+for file in file_names1:
+    _from = os.path.join(source, file)
+    _to = os.path.join(backup, file)
+    shutil.move(_from, _to)
+    
